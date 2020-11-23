1478ccc1151fdf9afc902e33c01ca6bec838756e
201810765Kimchaeyun <60181129+201810765Kimchaeyun@users.noreply.github.com>
2020-11-23 11:55:28 +0900

    Add files via upload

---
+++ emotionrecognition/java/ResultActivity.java
@@ -0,0 +1,50 @@
+package com.example.anany.emotionrecognition;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.microsoft.projectoxford.face.contract.Face;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        String data = getIntent().getStringExtra("list_faces");
+
+        Gson gson = new Gson();
+        Face[] faces = gson.fromJson(data, Face[].class);
+
+        ListView myListView = findViewById(R.id.listView);
+
+        byte[] byteArray = getIntent().getByteArrayExtra("image");
+        Bitmap orig = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        if (faces == null) {
+            if (data == null) {
+                Toast.makeText(getApplicationContext(), "Face array is null", Toast.LENGTH_LONG).show();
+            } else {
+
+            }
+        } else {
+            try {
+                CustomAdapter customAdapter = new CustomAdapter(faces, this, orig);
+                myListView.setAdapter(customAdapter);
+            } catch (Exception e) {
+                makeToast(e.getMessage());
+            }
+        }
+    }
+
+    private void makeToast(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    }
+
+
+}
